e84de86bea7b6f112bbd19204233a0718d8fa069
HenkieThee <103607679+HenkieThee@users.noreply.github.com>
2024-06-26 16:14:40 +0200

    eerste 3 paginas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/overons', function () {
+    return view('overons');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
