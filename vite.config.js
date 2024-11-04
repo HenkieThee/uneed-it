a5b1d65a0bf24118f449ec7e693f38fa86e8ea29
Neo_Tsang <neo@tsang.nu>
2024-11-04 13:34:44 +0100

    Small Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/accountstyle.css',
+                'resources/css/bestellenstyle.css',
+                'resources/css/app.css',
+                'resources/css/bezorg.css',
+                'resources/css/overonsstyle.css',
+                'resources/css/registrationstyle.css',
+                'resources/css/style.css',
+                'resources/css/style1.css',
+                'resources/css/verzoekenstyle.css',
+                'resources/css/zakelijkstyle.css',
+                'resources/js/app.js',
+                'resources/js/bootstrap.js' // Added from the screenshot
+            ],
             refresh: true,
         }),
     ],
