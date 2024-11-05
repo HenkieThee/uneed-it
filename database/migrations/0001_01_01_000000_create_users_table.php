8b01ffafabcfcab6a0d94aca802a09e27984491d
HenkieThee <krachtgames@gmail.com>
2024-11-05 17:36:39 +0100

    migrate aangepast

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::create('users', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->string('name');
-        //     $table->string('email')->unique();
-        //     $table->timestamp('email_verified_at')->nullable();
-        //     $table->string('password');
-        //     $table->rememberToken();
-        //     $table->timestamps();
-        // });
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('naam', 50);
+            $table->string('telefoonnummer', 15);
+            $table->string('email', 50)->unique();
+            $table->string('address', 50);
+            $table->string('password', 50);
+            $table->string('role', 20);
+            $table->timestamps();
+        });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
