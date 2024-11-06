5fb6329de5c39af751020240f7959258b7294593
HenkieThee <krachtgames@gmail.com>
2024-11-06 17:30:06 +0100

    category en pagination

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,11 +14,28 @@ class ProductSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
+        $categories = ['laptop', 'pc', 'onderdeel'];
 
-        for ($i = 0; $i < 25; $i++) {
+        for ($i = 0; $i < 75; $i++) {
+            $category = $faker->randomElement($categories);
+            $name = '';
+
+            switch ($category) {
+                case 'laptop':
+                    $name = $faker->words(1, true) . ' Laptop';
+                    break;
+                case 'pc':
+                    $name = $faker->words(1, true) . ' PC';
+                    break;
+                case 'onderdeel':
+                    $name = $faker->words(1, true) . ' Onderdeel';
+                    break;
+            }
+
             Product::create([
-                'name' => $faker->words(2, true) . ' Laptop',
+                'name' => $name,
                 'price' => $faker->randomFloat(2, 300, 2000),
+                'category' => $category,
                 'image' => 'images/pc.jpg',
             ]);
         }
