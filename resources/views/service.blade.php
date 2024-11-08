374a9cd3d8cb708932e297660fa4963f4a414f30
Anthony <9015167@student.zadkine.nl>
2024-11-08 15:15:23 +0100

    text aangepast service pagina

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -16,19 +16,19 @@
             <div class="Graphic-Design1">
                 <img src="" alt="">
                 <h1 class="Service-Text2">Software Development</h1>
-                <p class="Service-Paragraaf">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit dignissimos, facere consectetur, ipsam delectus voluptatibus reiciendis quas, </p>
+                <p class="Service-Paragraaf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi ab quo tempore? </p>
             </div>
             <div class="Graphic-Design">
                 <img src="" alt="">
                 <h1 class="Service-Text">Product Design</h1>
-                <p class="Service-Paragraaf">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nihil provident ipsa ut optio sunt tenetur vitae excepturi consequuntur est, </p>
+                <p class="Service-Paragraaf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi ab quo tempore? </p>
             </div>
         </div>
         <div class="Service-Align2">
             <div class="Graphic-Design1">
                 <img src="" alt="">
                 <h1 class="Service-Text2">Blog Writing</h1>
-                <p class="Service-Paragraaf">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora deleniti, dolorum dolore, </p>
+                <p class="Service-Paragraaf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi ab quo tempore? </p>
             </div>
             <div class="Graphic-Design">
                 <img src="" alt="">
@@ -38,7 +38,7 @@
             <div class="Graphic-Design1">
                 <img src="" alt="">
                 <h1 class="Service-Text2">IT Services</h1>
-                <p class="Service-Paragraaf">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, accusantium? </p>
+                <p class="Service-Paragraaf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi ab quo tempore? </p>
             </div>
         </div>
     </main>
