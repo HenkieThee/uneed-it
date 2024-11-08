3872ca055dd720f687b985dbd664868bdf233f68
HenkieThee <krachtgames@gmail.com>
2024-11-08 15:27:55 +0100

    voor anthony !

--- resources/views/login_or_signup.blade.php
+++ resources/views/login_or_signup.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container">
         <div class="login-signup">
-            <h1>Het lijkt erop dat jij niet ingelogd bent</h1>
+            <h1>Het lijkt erop dat jij niet ingelogd bent!</h1>
             <button onclick="window.location.href='{{ url('login') }}'">Log In</button>
             <button onclick="window.location.href='{{ url('registration') }}'">Sign Up</button>
         </div>
