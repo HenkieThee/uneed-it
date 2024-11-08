954ab3d49619c3e29735ee26759962d7b2ca3e31
Anthony <9015167@student.zadkine.nl>
2024-11-08 12:24:46 +0100

    home page change, footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <main style="background-image: url('{{ asset("images/40gp-uneedit5.jpg") }}');" id="mainhome">
         <div id="hometext">
             <img src="{{ asset('images/cropped-logo UNEED-IT(notext).png') }}">
-            <p><span class="white-text">voor al uw reparaties kunt u terecht bij </span><span class="red-text">Uneed-</span><span class="blue-text">it</span></p>
+            <p><span class="white-text">Voor al uw reparaties kunt u terecht bij </span><span class="red-text">UNEED-</span><span class="blue-text">IT</span></p>
         </div>
     </main>
 </x-app-layout>
@@ -12,24 +12,24 @@
         <div id="adress" class="flex items-center space-x-4">
             <img src="{{ asset('images/location.png') }}" class="w-8 h-8">
             <div>
-                <p>ZUIDBAAN 514, 2841MD</p>
-                <p>MOORDRECHT</p>
+            <p style="color: red;">ZUIDBAAN 514, 2841MD</p>
+                <p style="color: #48d1cc;">MOORDRECHT</p>
             </div>
         </div>
         <div id="telefoonnnumer" class="flex items-center space-x-4">
             <img src="{{ asset('images/phone.png') }}" class="w-8 h-8">
             <div>
-                <p>+316 30 985 409 SERVICENUMMER</p>
-                <p>+3118 28 202 18 KANTOOR</p>
-                <p>BEREIKBAAR VAN 09:00-18:00</p>
+                <p style="color: Red;" >+316 30 985 409 SERVICENUMMER</p>
+                <p style="color: #48d1cc;" >+3118 28 202 18 KANTOOR</p>
+                <p style="color: Red;" >BEREIKBAAR VAN 09:00-18:00</p>
             </div>
         </div>
         <div id="tijd" class="flex items-center space-x-4">
             <img src="{{ asset('images/clock.png') }}" class="w-8 h-8">
             <div>
-                <p>MA T/M VRIJ, 09:00 - 23:00</p>
-                <p>TELEFONISCH BEREIKBAAR</p>
-                <p>VOOR ABONNEMENTHOUDERS</p>
+                <p style="color: #48d1cc;">MA T/M VRIJ, 09:00 - 23:00</p>
+                <p style="color: Red;" >TELEFONISCH BEREIKBAAR</p>
+                <p style="color: #48d1cc;">VOOR ABONNEMENTHOUDERS</p>
             </div>
         </div>
     </div>
